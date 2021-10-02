f67b6acf352ebf22b17b26a22a48137403000efc
jdselsor <jdselsor@gmail.com>
2021-10-01 17:00:03 -0700

    Added json parsing libary

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <jsoncpp/json/json.h>
 
 #include <iostream>
-
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
-float x = 0.0f;
+#include <fstream>
 
 void pollInput ();
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My window");
-    sf::RectangleShape shape(sf::Vector2f(120.f, 50.f));
-    shape.setPosition (400.0f - 60.0f, 300.0f - 25.0f);
+    std::ifstream ifs("./assets/config.json");
+    Json::Reader reader;
+    Json::Value obj;
+    reader.parse(ifs, obj);
 
-    sf::View view2(sf::FloatRect(0.f, 0.f, (float) WINDOW_WIDTH, (float) WINDOW_HEIGHT));
+    std::cout << "Fullscreen: " << obj["fullscreen"].asString() << std::endl;
+    std::cout << "Width: " << obj["width"].asUInt() << ", height: " << obj["height"].asUInt() << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -29,43 +31,9 @@ int main()
                 window.close();
         }
 
-        pollInput();
-
-        view2.setRotation(x);
-
         window.clear();
-
-        window.setView (view2); // This updates the view.
-        window.draw(shape);
-
         window.display();
     }
 
     return 0;
-}
-
-void pollInput ()
-{
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-    {
-        // left key is pressed: move our character
-        //std::cout << "Left Key Pressed" << std::endl;
-        x -= 0.05f;
-        std::cout << "X: " << x << std::endl;
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-    {
-        // left key is pressed: move our character
-        //std::cout << "Left Key Pressed" << std::endl;
-        x += 0.05f;
-        std::cout << "X: " << x << std::endl;
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
-    {
-        // left key is pressed: move our character
-        //std::cout << "Left Key Pressed" << std::endl;
-        x = 0.0f;
-    }
 }
